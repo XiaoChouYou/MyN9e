07e661f17c8a385344989d4e4a03a06ddb3c6e27
lqz <315457522@qq.com>
2022-10-03 01:29:32 +0800

    开始测试

--- docker/build_cmd/prometheus.sh
+++ docker/build_cmd/prometheus.sh
@@ -1,11 +1,14 @@
 
 
 docker run -it --rm  --name prometheus-demo  -d prom/prometheus:v2.33.4
+
+
 if [  ! -d ${PROJECTHOME}/prometheus ]
 then
   mkdir -p ${PROJECTHOME}/prometheus
 fi
-docker cp prometheus-demo:/prometheus        ${PROJECTHOME}/prometheus/data
-docker cp prometheus-demo:/etc/prometheus    ${PROJECTHOME}/prometheus/etc
+docker cp prometheus-demo:/prometheus/        ${PROJECTHOME}/prometheus/data/
+docker cp prometheus-demo:/etc/prometheus/    ${PROJECTHOME}/prometheus/etc/
+
 chmod -R 777  ${PROJECTHOME}/prometheus/data ${PROJECTHOME}/prometheus/etc
 docker stop prometheus-demo
