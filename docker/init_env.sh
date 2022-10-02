7a3c0a9af7c2910941025cf9e0487a9c28dbd9b1
lqz <315457522@qq.com>
2022-10-02 21:08:13 +0800

    开始测试

--- docker/init_env.sh
+++ docker/init_env.sh
@@ -13,21 +13,35 @@ export SUBNETWORK=172.0.0.0/24
 
 
 # DNS服务器IP
-export DNSSERVICE=172.0.0.250
-export DNSUSER=admin
-export DNSPASSWD=123123
+export DNS_SERVICE=172.0.0.250
+export DNS_USER=admin
+export DNS_PASSWD=123123
 sh  "${WORKHOME}"/build_cmd/dns.sh
 
-# POSTGRES数据库
-export POSTGRESPASSWD=123123
+# timescaledb数据库
+export POSTGRES_SERVICE=172.0.0.2
+export POSTGRES_PASSWD=123123
+export POSTGRES_USER=postgres
+export POSTGRES_DB=postgres
 export PROC_GID=1000
 export PROC_UID=1000
 sh  "${WORKHOME}"/build_cmd/timescaledb.sh
 
 
+# PROMSCALE 数据库
+export PROMSCALE_SERVICE=172.0.0.3
+export PROMSCALE_DB_HOST=MyPostgres
+export PROMSCALE_DB_PORT=5432
+export PROMSCALE_DB_SSL_MODE=allow
+export PROMSCALE_DB_NAME=${POSTGRES_DB}
+export PROMSCALE_DB_PASSWORD=${POSTGRES_PASSWD}
+export WAIT_HOSTS=timescaledb:5432
+
+
+
 #NGINX配置
-export NGINXSERVICE=172.0.0.100
-export NGINXHTTPPORT=10080
-export NGINXHTTPsPORT=10443
+export NGINX_SERVICE=172.0.0.100
+export NGINX_HTTPPORT=10080
+export NGINX_HTTPsPORT=10443
 sh  "${WORKHOME}"/build_cmd/nginx.sh
 
