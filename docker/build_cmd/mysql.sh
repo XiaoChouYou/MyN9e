467c3b5f71560b90d995777fdbe2f11183352de4
lqz <315457522@qq.com>
2022-10-03 14:47:14 +0800

    开始测试

--- docker/build_cmd/mysql.sh
+++ docker/build_cmd/mysql.sh
@@ -17,4 +17,13 @@ then
 fi
 
 
+docker run -it --rm  --name mysql-demo  -d xiaochouyou/mysql:5.7
+if [  ! -d ${PROJECTHOME}/mysql/etc/ ]
+then
+  docker cp prometheus-demo:/etc/mysql/*        ${PROJECTHOME}/mysql/etc/
+fi
+chmod -R 777  ${PROJECTHOME}/mysql/etc/
+docker stop mysql-demo
+
+
 cp ${WORKHOME}/build_cmd/initsql_for_mysql/*sql ${PROJECTHOME}/mysql/initsql
