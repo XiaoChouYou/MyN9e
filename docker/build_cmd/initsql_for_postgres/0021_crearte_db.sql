58ec273c79979100edb47806b2ac69f21bca0f45
lqz <315457522@qq.com>
2022-10-03 12:46:27 +0800

    开始测试

---
+++ docker/build_cmd/initsql_for_postgres/0021_crearte_db.sql
@@ -0,0 +1,21 @@
+
+
+create user promscale with password 'promscale';
+create database promscale owner  promscale ;
+grant all privileges on database promscale to promscale;
+
+
+create user n9e with password 'n9e';
+create database n9e owner  n9e ;
+grant all privileges on database n9e to n9e;
+
+
+create user ibex with password 'ibex';
+create database ibex owner  ibex ;
+grant all privileges on database ibex to ibex;
+
+
+
+create user kong with password 'kong';
+create database kong owner  kong ;
+grant all privileges on database kong to kong;
