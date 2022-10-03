55723416fdf4631d7e80737cb09698c8be5539a7
lqz <315457522@qq.com>
2022-10-03 15:50:53 +0800

    开始测试

--- docker/build_cmd/n9e.sh
+++ docker/build_cmd/n9e.sh
@@ -143,12 +143,12 @@ Password = "${REDIS_PASSWORD}"
 [DB]
 # postgres: host=%s port=%s user=%s dbname=%s password=%s sslmode=%s
 # DSN="root:1234@tcp(mysql:3306)/n9e_v5?charset=utf8mb4&parseTime=True&loc=Local&allowNativePasswords=true"
-host=${N9E_PGDB_HOST}
-port=${N9E_PGDB_PORT}
-user=${N9E_PGDB_USER}
-dbname=${N9E_PGDB_NAME}
-password=${N9E_PGDB_PASSWORD}
-sslmode=${N9E_PGDB_SSL_MODE}
+host="${N9E_PGDB_HOST}"
+port="${N9E_PGDB_PORT}"
+user="${N9E_PGDB_USER}"
+dbname="${N9E_PGDB_NAME}"
+password="${N9E_PGDB_PASSWORD}"
+sslmode="${N9E_PGDB_SSL_MODE}"
 # enable debug mode or not false
 Debug = enable
 # mysql postgres
