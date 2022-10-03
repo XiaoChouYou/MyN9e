52d81e644e1daa1e31a67720fa03fb948f08abbb
lqz <315457522@qq.com>
2022-10-03 14:32:21 +0800

    开始测试

--- docker/build_cmd/initsql_for_mysql/c-init.sql
+++ docker/build_cmd/initsql_for_mysql/c-init.sql
@@ -1,3 +1,11 @@
 GRANT ALL ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '1234';
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '1234';
 GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '1234';
+
+GRANT ALL ON *.* TO 'n9e_v5'@'127.0.0.1' IDENTIFIED BY 'n9e_v5';
+GRANT ALL ON *.* TO 'n9e_v5'@'localhost' IDENTIFIED BY 'n9e_v5';
+GRANT ALL ON *.* TO 'n9e_v5'@'%' IDENTIFIED BY 'n9e_v5';
+
+GRANT ALL ON *.* TO 'ibex'@'127.0.0.1' IDENTIFIED BY 'ibex';
+GRANT ALL ON *.* TO 'ibex'@'localhost' IDENTIFIED BY 'ibex';
+GRANT ALL ON *.* TO 'ibex'@'%' IDENTIFIED BY 'ibex';
