3eeec007473b9c4c736c1fee81633b11bfc90a43
lqz <315457522@qq.com>
2022-10-07 11:10:42 +0800

    开始测试

---
+++ docker/build_cmd/grafana.sh
@@ -0,0 +1,43 @@
+
+echo "init .. grafana9 .. "
+docker run -it --rm  --name grafana9-demo  -d xiaochouyou/grafana:9.1.7
+
+
+if [  ! -d ${PROJECTHOME}/grafana9 ]
+then
+  mkdir -p ${PROJECTHOME}/grafana9
+fi
+if [  ! -d ${PROJECTHOME}/grafana9/etc/ ]
+then
+  docker cp grafana9-demo:/etc/grafana/        ${PROJECTHOME}/grafana9/etc/
+fi
+
+if [  ! -d ${PROJECTHOME}/grafana9/lib/ ]
+then
+  docker cp grafana9-demo:/var/lib/grafana/        ${PROJECTHOME}/grafana9/lib/
+fi
+
+chmod -R 777  ${PROJECTHOME}/grafana9/
+docker stop grafana9-demo
+
+
+echo "init .. grafana8 .. "
+docker run -it --rm  --name grafana8-demo  -d xiaochouyou/grafana:8.5.13
+
+
+if [  ! -d ${PROJECTHOME}/grafana8 ]
+then
+  mkdir -p ${PROJECTHOME}/grafana8
+fi
+if [  ! -d ${PROJECTHOME}/grafana8/etc/ ]
+then
+  docker cp grafana8-demo:/etc/grafana/        ${PROJECTHOME}/grafana8/etc/
+fi
+
+if [  ! -d ${PROJECTHOME}/grafana8/lib/ ]
+then
+  docker cp grafana8-demo:/var/lib/grafana/        ${PROJECTHOME}/grafana8/lib/
+fi
+
+chmod -R 777  ${PROJECTHOME}/grafana8/
+docker stop grafana8-demo
