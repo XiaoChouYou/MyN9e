126a681a4ca6db2f6f8609d88107127ec27cf1cb
lqz <315457522@qq.com>
2022-10-07 13:14:07 +0800

    开始测试

--- docker/build_cmd/nginx.sh
+++ docker/build_cmd/nginx.sh
@@ -1,2 +1,19 @@
 
 
+
+echo "init .. nginx .. "
+docker run -it --rm  --name nginx-demo  -d xiaochouyou/nginx:1.23.1-alpine
+
+
+if [  ! -d ${PROJECTHOME}/nginx/ ]
+then
+  mkdir -p ${PROJECTHOME}/nginx/
+fi
+if [  ! -d ${PROJECTHOME}/nginx/etc/ ]
+then
+  docker cp nginx-demo:/etc/nginx/        ${PROJECTHOME}/nginx/etc/
+fi
+
+
+docker stop nginx-demo
+
