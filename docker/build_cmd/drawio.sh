26ab809c0531b3b600dfcacb5e13392e42aa3bd9
lqz <315457522@qq.com>
2022-10-07 11:31:46 +0800

    开始测试

--- docker/build_cmd/drawio.sh
+++ docker/build_cmd/drawio.sh
@@ -7,9 +7,14 @@ if [  ! -d ${PROJECTHOME}/drawio ]
 then
   mkdir -p ${PROJECTHOME}/drawio
 fi
-if [  ! -d ${PROJECTHOME}/drawio/tomcat/ ]
+if [  ! -d ${PROJECTHOME}/drawio/conf/ ]
 then
-  docker cp drawio-demo:/usr/local/tomcat/        ${PROJECTHOME}/drawio/tomcat/
+  docker cp drawio-demo:/usr/local/tomcat/conf/        ${PROJECTHOME}/drawio/conf/
+fi
+
+if [  ! -d ${PROJECTHOME}/drawio/webapps/ ]
+then
+  docker cp drawio-demo:/usr/local/tomcat/webapps        ${PROJECTHOME}/drawio/webapps/
 fi
 
 chmod -R 777  ${PROJECTHOME}/drawio/tomcat/
